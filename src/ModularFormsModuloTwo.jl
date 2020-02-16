21bc880df03c4c162354ffc38a2151fd1a5005d2
pauldubois98 <pauldubois98@gmail.com>
2020-02-17 00:46:56 +0100

    added modular form generators

--- src/ModularFormsModuloTwo.jl
+++ src/ModularFormsModuloTwo.jl
@@ -6,6 +6,7 @@ module ModularFomrsModuloTwo
     import Base: +, *, ^
 
     include("arithmetic.jl")
+    include("generators.jl")
 
 
 
---
+++ src/generators.jl
@@ -0,0 +1,109 @@
+"""
+This file contains routines to generete standard modular forms mod 2.
+Essentialy, this is 0 form, 1 form, and powers of Δ.
+"""
+
+
+
+#standart modular forms mod 2 initalizers
+"""
+    zero([LENGTH])
+
+Create a zero form of length LENGTH
+
+# Example
+```julia-repl
+julia> zero()
+1000-element SparseVector{Int8,Int64} with 0 stored entries
+
+julia> zero(1)
+1-element SparseVector{Int8,Int64} with 0 stored entries
+```
+"""
+function zero(LENGTH::Int=10^3)::SparseVector{Int8,Int}
+    return spzeros(Int8, LENGTH)
+end
+
+"""
+    one([LENGTH])
+
+Create a one form of length LENGTH
+
+# Example
+```julia-repl
+julia> one()
+1000-element SparseVector{Int8,Int64} with 1 stored entry:
+  [1   ]  =  1
+
+julia> one(1)
+1-element SparseVector{Int8,Int64} with 1 stored entry:
+  [1]  =  1
+```
+"""
+function one(LENGTH::Int=10^3)::SparseVector{Int8,Int}
+    f = spzeros(Int8, LENGTH)
+    f[1] = 1
+    return f
+end
+
+"""
+    delta([LENGTH])
+
+Create the standard Δ form, with coefficients up to LENGTH
+
+# Example
+```julia-repl
+julia> disp(delta())
+MF mod 2 (coef to 1000) - 01000000010000000000000001000000000000000000000001...
+
+julia> disp(delta(10^6))
+MF mod 2 (coef to 1000000) - 01000000010000000000000001000000000000000000000001...
+```
+"""
+function delta(LENGTH::Int=10^3)::SparseVector{Int8,Int}
+    f = spzeros(Int8, LENGTH)
+    indice::Int = 2
+    i::Int = 1
+    while indice <= f.n
+        f[indice] = Int8(1)
+        i += 2
+        indice = i^2+1
+    end
+    return f
+end
+
+"""
+    delta_k(k[, LENGTH])
+
+Create the standard Δ^k form, with coefficients up to LENGTH
+
+# Example
+```julia-repl
+julia> disp(delta_k(0))
+MF mod 2 (coef to 1000) - 10000000000000000000000000000000000000000000000000...
+
+julia> disp(delta_k(1))
+MF mod 2 (coef to 1000) - 01000000010000000000000001000000000000000000000001...
+
+julia> disp(delta_k(2))
+MF mod 2 (coef to 1000) - 00100000000000000010000000000000000000000000000000...
+
+julia> disp(delta_k(3))
+MF mod 2 (coef to 1000) - 00010000000100000001000000000000000000000001000000...
+
+julia> disp(delta_k(5))
+MF mod 2 (coef to 1000) - 00000100000001000000000000000100000001000000010000...
+```
+"""
+
+function delta_k(k::Int, LENGTH::Int=10^3)::SparseVector{Int8,Int}
+    if k==0
+        return one(LENGTH)
+    elseif k==1
+        return delta(LENGTH)
+    elseif k==2
+        return sq(delta(LENGTH))
+    else
+        return delta(LENGTH)^k
+    end
+end
